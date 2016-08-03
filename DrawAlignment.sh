a73ef6254efcc2752364c2f960a3efaed9c5f550
jamesdgalbraith <galbraithenator@gmail.com>
2016-08-03 10:22:27 +0930

    Now draws two plots with two word lengths

--- DrawAlignment.sh
+++ DrawAlignment.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Using gepard constructs self-alignment pngs of sequences
+# 
+
+
+
 #SBATCH -p batch
 #SBATCH -N 1
 #SBATCH -n 4
@@ -16,6 +21,8 @@ module load Java/1.8.0_71
 while read "name"
 do
 
-	java -cp ~/Workspace/gepard/dist/Gepard-1.40.jar org.gepard.client.cmdline.CommandLine -seq1 ~/Workspace/Cluster/AlignmentFASTAs/${name} -seq2 ~/Workspace/Cluster/AlignmentFASTAs/${name} -matrix ~/Workspace/gepard/resources/matrices/edna.mat -outfile ~/Workspace/Cluster/AlignmentPNGs/${name}.png
+        # -seq1 and seq2  are the path to the sequence, -matrix the path to the matrix and -outfile the location for the constructed png 
+        java -cp ~/Workspace/gepard/dist/Gepard-1.40.jar org.gepard.client.cmdline.CommandLine -seq1 ~/Workspace/Cluster/AlignmentFASTAs/${name} -seq2 ~/Workspace/Cluster/AlignmentFASTAs/${name} -matrix ~/Workspace/gepard/resources/matrices/edna.mat -outfile ~/Workspace/Cluster/AlignmentPNGs6/${name}6.png -word 6
+        java -cp ~/Workspace/gepard/dist/Gepard-1.40.jar org.gepard.client.cmdline.CommandLine -seq1 ~/Workspace/Cluster/AlignmentFASTAs/${name} -seq2 ~/Workspace/Cluster/AlignmentFASTAs/${name} -matrix ~/Workspace/gepard/resources/matrices/edna.mat -outfile ~/Workspace/Cluster/AlignmentPNGs9/${name}9.png -word 9
 
-done <"/home/a1194388/Workspace/Cluster/repeatfilenames"
+done <"/home/a1194388/Workspace/Cluster/FASTANames.txt"
